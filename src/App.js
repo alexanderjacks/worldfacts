93c97ee2cf90cb649c7daeede6c199af93d0bf71
alexanderjacks <alexanderthejacks@gmail.com>
2018-11-11 12:17:35 -0800

    :triangular_flag_on_post: renders mapped data sample from local JSON

--- src/App.js
+++ src/App.js
@@ -16,60 +16,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>
-            React Dataviz Templ8
+            🌐 Worldfacts w/ D3
           </h1>
+          <h4>
+            React is made for engineers 🤓
+          </h4>
+          <h5>
+            But designers can learn to use it, too 👨‍🎤
+          </h5>
           <div className="row">
-            <div className="col">
-              <a href="http://www.alexanderjacks.info">
-                <img
-                  src="images/bizcard.png"
-                  className="img-fluid"
-                  alt="my business card"
-                 /><br/>
-                 <h5>
-                  By Alexander Jacks
-                 </h5>
-                </a>
-            </div>
-            <div className="d-flex justify-content-around flex-column col-md-6">
-              <div className="mx-auto">
-                <BasicPopup
-                  btnText="Companies I've worked with"
-                  title="Proud to consult with..."
-                  text="Minority Veterans of America, Blackwood Housekeeping, JENIE.com, Veteran Mentor Network, Sitecast.com, Shadowcreek Software, WizFish Studios"
-                  confirmButtonText="Outsanding!"
-                />
-                <BasicPopup
-                  btnText="About this Templ8"
-                  btnColor="orange"
-                  title="Front-end Development Kit"
-                  text="Styling library MDBootstrap included. Some Nivo modules showcased. FontAwesome5 CDN set up. SweetAlert components ready to use. Axios calls a local JSON (it's sourced from https://www.cia.gov/library/publications/the-world-factbook/)"
-                  confirmButtonText="Way to Be! ✌️"
-                />
-                <BasicPopup
-                  btnText="More about Alex"
-                  btnColor="purple"
-                  title="My Journey in Tech, in Brief"
-                  text="I built my first website in 1997, and figured out how to host it on the local ISP after I read enough BBS posts about <strong><em>FTP</em></strong> clients. I decided I wanted to do this professionally, so a few years later I enrolled in a Web Programming school in NYC and learned industry best-practices and even built my own <strong><em>GUI</em></strong> in <strong><em>Java</em></strong>! Then 9/11 went down, and I was traumatised and angry enough to join the US Air Force, where I began an intense 5-year career as a <strong><em>data and voice network sysadmin</em></strong>. Years later I used the 9/11 GI Bill to complete a Psychology degree, but I still missed working with technology and designing user experiences. <p>So in 2017 I finished a code school in Portland, Oregon and learned how to work with current JS frameworks like <strong><em>React</em></strong>, <strong><em>Vue</em></strong>, <strong><em>Angular</em></strong>, (even <strong><em>Ember</em></strong>, in theory!) and how to leverage tools like <strong><em>Github</em></strong>, <strong><em>Bootstrap</em></strong>, <strong><em>Sass</em></strong>, <strong><em>FontAwesome</em></strong>, and more. I've been consulting as a web developer since 2015 (before code school, even) and am searching for a full time role in the Portland, Oregon area so I can supercharge my front-end learnings with some mentorship and <strong><em>Agile dev</em></strong>. Right now I'm volunteering with Code4PDX which is tuning me in to team development practices, and I'm also volunteering to build a site for Minority Veterans of America, which is helping me learn the <strong><em>Agile</em></strong> methodology and become comfy doing code reviews and PRs on <strong><em>Github</em></strong>. Thanks for reading all of this!</p>"
-                  confirmButtonText="Right on! 🙏"
-                />
-              </div>
-            </div>
+            
           </div>
         </header>
         <div className="important blk-frame">
           <Factbook />
         </div>
-        <div className="nivocalendar blk-frame">
-          <NivoCalendar />
-        </div>
-        <h4>Nivo Calendar
-        </h4>
-        <div className="nivonoi blk-frame">
-          <NivoVoronoi />
-        </div>
-        <h5>Nivo Voronoi
-        </h5>
         <div className="nivowaffle blk-frame">
           <NivoWaffle />
         </div>
--- src/Factbook.js
+++ src/Factbook.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, List} from 'react';
 import axios from 'axios';
 
 class Factbook extends Component {
@@ -6,21 +6,37 @@ class Factbook extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			users: [],
-			store: []
+			store: [],
+			nations: []
 		}
 	}
 
 	componentDidMount() {
 	    axios.get('factbook.json')
-	    	.then(json => console.log(json.data.countries[0].data.introduction))
+	    	.then(json => json.data.nations.map(nation => (
+	    	{
+	    		name: `${nation.data.name}`,
+	    		geo: `${nation.data.geography.area.total.value}`,
+	    		intro: `${nation.data.introduction.background}`
+	    	})))
+	    	.then(newData => this.setState({nations: newData}))
   	}
 	
 	render () {
 		return (
 			<div>
-				<h2>Get your emojis ready 😈🇨🇦
+				<h2>Nations/Regions of the World
 				</h2>
+				<ul className="list-group">
+					{this.state.nations.map(function(nation, i){
+						return <li key={i} className="list-group-item">
+							<h5>{nation.name}</h5>
+							<h6>{nation.geo} sq.km </h6>
+							<p className="tiny-text">{nation.intro}</p>
+						</li>
+					}
+					)}
+				</ul>
 			</div>
         );
 	}
